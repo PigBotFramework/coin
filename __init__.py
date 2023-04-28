5d3d99084a68211125cf578d4c08502ea131eac0
gingmzmzx <2417481092@qq.com>
2023-04-29 01:30:08 +0800

    定时任务自动push

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-import requests, time, cache
-from pbf import PBF
-from utils.RegCmd import RegCmd
-from utils.cqcode import CQCode
-from utils.coin import Coin
+import requests, time
+from pbf.controller import Cache
+from pbf.controller.PBF import PBF
+from pbf.utils.RegCmd import RegCmd
+from pbf.utils.CQCode import CQCode
+from pbf.utils.Coin import Coin
 
 _name = "好感度系统扩展"
 _version = "1.0.1"
@@ -13,46 +14,38 @@ _cost = 0.00
 class coin(PBF):
     def __enter__(self):
         return [
-            RegCmd(
-                name = "绑定频道",
-                usage = "绑定频道",
-                permission = "anyone",
-                function = "coin@bangding",
-                description = "将正常QQ号与频道绑定",
-                mode = "好  感  度",
-                hidden = 0,
-                type = "command"
-            ),
-            RegCmd(
-                name = "注册",
-                usage = "注册",
-                permission = "anyone",
-                function = "coin@zhuce",
-                description = "注册用户",
-                mode = "好  感  度",
-                hidden = 0,
-                type = "command"
-            ),
-            RegCmd(
-                name = "投食",
-                usage = "投食",
-                permission = "anyone",
-                function = "coin@toushi",
-                description = "喂一喂小猪比",
-                mode = "好  感  度",
-                hidden = 0,
-                type = "command"
-            ),
-            RegCmd(
-                name = "加好感度 ",
-                usage = "加好感度 <QQ号或者@那个人>[ <要加的个数，不指定则随机数>]",
-                permission = "owner",
-                function = "coin@addCoinFunc",
-                description = "给用户加好感度",
-                mode = "好  感  度",
-                hidden = 0,
-                type = "command"
-            )
+    @RegCmd(
+        name = "绑定频道",
+        usage = "绑定频道",
+        permission = "anyone",
+        function = "coin@bangding",
+        description = "将正常QQ号与频道绑定",
+        mode = "好  感  度"
+    )
+    @RegCmd(
+        name = "注册",
+        usage = "注册",
+        permission = "anyone",
+        function = "coin@zhuce",
+        description = "注册用户",
+        mode = "好  感  度"
+    )
+    @RegCmd(
+        name = "投食",
+        usage = "投食",
+        permission = "anyone",
+        function = "coin@toushi",
+        description = "喂一喂小猪比",
+        mode = "好  感  度"
+    )
+    @RegCmd(
+        name = "加好感度 ",
+        usage = "加好感度 <QQ号或者@那个人>[ <要加的个数，不指定则随机数>]",
+        permission = "owner",
+        function = "coin@addCoinFunc",
+        description = "给用户加好感度",
+        mode = "好  感  度"
+    )
         ]
     
     def bangding(self):
