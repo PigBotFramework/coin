368dca00db8e4e89d267e9f9bb30c8910e6f8c6e
gingmzmzx <2417481092@qq.com>
2023-04-29 18:39:40 +0800

    定时任务自动push

--- __init__.py
+++ __init__.py
@@ -12,42 +12,13 @@ _author = "xzyStudio"
 _cost = 0.00
 
 class coin(PBF):
-    def __enter__(self):
-        return [
     @RegCmd(
         name = "绑定频道",
         usage = "绑定频道",
         permission = "anyone",
-        function = "coin@bangding",
         description = "将正常QQ号与频道绑定",
         mode = "好  感  度"
     )
-    @RegCmd(
-        name = "注册",
-        usage = "注册",
-        permission = "anyone",
-        function = "coin@zhuce",
-        description = "注册用户",
-        mode = "好  感  度"
-    )
-    @RegCmd(
-        name = "投食",
-        usage = "投食",
-        permission = "anyone",
-        function = "coin@toushi",
-        description = "喂一喂小猪比",
-        mode = "好  感  度"
-    )
-    @RegCmd(
-        name = "加好感度 ",
-        usage = "加好感度 <QQ号或者@那个人>[ <要加的个数，不指定则随机数>]",
-        permission = "owner",
-        function = "coin@addCoinFunc",
-        description = "给用户加好感度",
-        mode = "好  感  度"
-    )
-        ]
-    
     def bangding(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
@@ -65,6 +36,13 @@ class coin(PBF):
             
             cache.refreshFromSql('userCoin')
     
+    @RegCmd(
+        name = "投食",
+        usage = "投食",
+        permission = "anyone",
+        description = "喂一喂小猪比",
+        mode = "好  感  度"
+    )
     def toushi(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
@@ -96,6 +74,13 @@ class coin(PBF):
         
         cache.refreshFromSql('userCoin')
     
+    @RegCmd(
+        name = "注册",
+        usage = "注册",
+        permission = "anyone",
+        description = "注册用户",
+        mode = "好  感  度"
+    )
     def zhuce(self, sendFlag=1):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
@@ -122,6 +107,13 @@ class coin(PBF):
         
         cache.refreshFromSql('userCoin')
             
+    @RegCmd(
+        name = "加好感度 ",
+        usage = "加好感度 <QQ号或者@那个人>[ <要加的个数，不指定则随机数>]",
+        permission = "owner",
+        description = "给用户加好感度",
+        mode = "好  感  度"
+    )
     def addCoinFunc(self):
         uid = self.data.se.get('user_id')
         gid = self.data.se.get('group_id')
